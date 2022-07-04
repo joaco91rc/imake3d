26859593e77469add8c94385cfbf4f87031b51e8
Joaco Alvarez <joaquinaalvarez91@gmail.com>
2022-07-04 14:10:59 -0300

    Se agrega logo en la NavBar

--- src/components/navegacion/Navbar.js
+++ src/components/navegacion/Navbar.js
@@ -6,7 +6,9 @@ const navbar = () => {
     <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">IMake3D</a>
+    <Link to='/'>
+        <img src='./logoNavbar.png' width='50'/>
+      </Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
