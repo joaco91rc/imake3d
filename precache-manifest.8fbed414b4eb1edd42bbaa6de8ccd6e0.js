92db9119cf3c6d5eb73a139a051d1916cf4e7782
Joaco Alvarez <jaalvarez@cgp.gba.gov.ar>
2022-08-08 08:26:38 -0300

    Updates

---
+++ precache-manifest.8fbed414b4eb1edd42bbaa6de8ccd6e0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "78f22d74908f1f3a5084",
+    "url": "/imake3dAlvarez/static/css/main.dcce39e1.chunk.css"
+  },
+  {
+    "revision": "78f22d74908f1f3a5084",
+    "url": "/imake3dAlvarez/static/js/main.64fa3cd3.chunk.js"
+  },
+  {
+    "revision": "9fa2f541dd83b8304aad",
+    "url": "/imake3dAlvarez/static/js/runtime~main.0844be8e.js"
+  },
+  {
+    "revision": "223b5468b669241b5fd6",
+    "url": "/imake3dAlvarez/static/css/2.eedce737.chunk.css"
+  },
+  {
+    "revision": "223b5468b669241b5fd6",
+    "url": "/imake3dAlvarez/static/js/2.ee2b78e6.chunk.js"
+  },
+  {
+    "revision": "60345cd76b331792ecba",
+    "url": "/imake3dAlvarez/static/js/3.e286ee84.chunk.js"
+  },
+  {
+    "revision": "b4d350927c8cfa0ef05ac32adb296a55",
+    "url": "/imake3dAlvarez/static/media/iconoIG.b4d35092.png"
+  },
+  {
+    "revision": "f73daf3d24ac5ace22b83f996c5d4cff",
+    "url": "/imake3dAlvarez/static/media/iconoWSP.f73daf3d.png"
+  },
+  {
+    "revision": "bea5c5e4bb7696896cee07322db91bc1",
+    "url": "/imake3dAlvarez/index.html"
+  }
+];
